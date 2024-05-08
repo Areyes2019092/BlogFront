8ace154be338b458d6e565ecc20b22a00c993a02
Angel <areyes-2019092@kinal.edu.gt>
2024-05-08 16:39:13 -0600

    Footer and categories css

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
 
 const Footer = () => {
   return (
     <footer>
       <ul className="footer__categories">
-        <li><link to="/posts/categories/:"></link></li>
+        <li><Link to="/posts/categories/Agriculture">Taller</Link></li>
+        <li><Link to="/posts/categories/Business">Practica Supervisada</Link></li>
+        <li><Link to="/posts/categories/Education">Tegnologia</Link></li>
       </ul>
+      <div className="footer__copyright">
+        <small>Blog Angel Reyes</small>
+      </div>
     </footer>
   )
 }
